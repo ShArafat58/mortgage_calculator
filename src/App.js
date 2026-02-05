daa06bb80ecd06b0c1ef4ca5873c454fe865d8e8
Shahriar Hossain Arafat <shahriararafat20@gmail.com>
2026-02-06 01:09:28 +0600

    update: modified App.js and added components folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { Grid } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Results from "./components/Results";
+import SliderSelect from "./components/SliderSelect";
+import Tenure from "./components/Tenure";
 
 function App() {
   return (
     <div className="App">
-      hello everyone
+      <Navbar/>
+      <Grid container spacing={5}>
+        <Grid item>
+          <SliderSelect/>
+          <Tenure/>
+        </Grid>
+        <Grid item>
+          <Results/>
+        </Grid>
+      </Grid>
     </div>
   );
 }
